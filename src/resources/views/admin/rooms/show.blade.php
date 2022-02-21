f86321f755810000668e711b51b990e428f97bfe
kelnid <xopoho2222@gmail.com>
2022-02-21 14:47:21 +0200

    another

--- src/resources/views/admin/rooms/show.blade.php
+++ src/resources/views/admin/rooms/show.blade.php
@@ -11,7 +11,7 @@
                     <div class="card-body">
                         <h5 class="card-title">{{ $room->name }}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">{{ $room->hotel->name }}</h6>
-                        <p class="card-text">{{ $room->area }}</p>
+                        <p class="card-text">Площадь: {{ $room->area }} м²</p>
                         <p class="card-text">{{ $room->bed }}</p>
                         <a href="{{ route('admin.rooms.edit', ['room' => $room->id]) }}" class="card-link">Редактировать</a>
                         <form action="{{ route('admin.rooms.destroy', ['room' => $room->id]) }}" method="POST">
--- src/resources/views/user/rooms/show.blade.php
+++ src/resources/views/user/rooms/show.blade.php
@@ -12,7 +12,7 @@
                         <h5 class="card-title">{{ $room->name }}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">{{ $room->hotel->name }}</h6>
                         <p class="card-text">{{ $room->area }}</p>
-                        <p class="card-text">{{ $room->bed }}</p>
+                        <p class="card-text">Площадь: {{ $room->area }} м²</p>
                         <a href="#" class="card-link">Забронировать</a>
                     </div>
                 </div>
